b777b135bd9ed0de17d117d7f175024fca9fdc38
andy <altenfreelance@gmail.com>
2019-12-07 11:40:51 -0500

    Added new bio

--- index.php
+++ index.php
@@ -76,11 +76,29 @@ $nav = new Nav();
 echo $nav;
 ?>
                             <h2>About the Band</h1>
-                            <p>Rooster Ridge is an American folk and bluegrass band from Loveland, Ohio. The band was established
+                            <p>
+                            Blending cutting-edge bluegrass and folk with the dulcet tones of traditional Americana, Rooster Ridge is an ascendant voice in the Ohio 
+                            music scene.  Reminiscent of classic groups like The Band, The Stanley Brothers, Nitty Gritty Dirt Band, and more recently Old Crow 
+                            Medicine Show, Rooster Ridge brings together superior instrumentation, contagious melodies, harmonies as smooth as Kentucky bourbon,
+                            and lyrics as frank and honest as the countryside to offer a distinctive new sound that still holds firm to its roots.
+                            </p>
+
+ 
+                            <p>Rooster Ridge was founded in Loveland, Ohio by the Alten Brothers, Jake and Andrew, and the father-son team of Greg and Vince Stevens.
+                            Rounding out the group are close friends and lead vocalists Joey Oberholzer and Bryce Clawson, all anchored by Dave Blumberg’s 
+                            foundational bass lines.
+                            </p>
+ 
+                            <p>Starting out quite literally as a late-night, back-porch jam session, Rooster Ridge has begun to catch fire, drawing ever larger
+                            crowds and an exploding local fan base as they play a growing universe of venues.  Rooster Ridge thanks all of their supporters 
+                            for an exceptional start to what many listeners believe will be a promising and prolific future.  Rooster Ridge invites you to sit
+                            back, relax, and become part of their distinctly American musical journey.
+                            </p>
+                            <!-- <p>Rooster Ridge is an American folk and bluegrass band from Loveland, Ohio. The band was established
                             in 2018 and is made up of members Andrew Alten, Jacob Alten, Vince Stevens, Joey Oberholzer, Bryce
                              Clawson, Greg Stevens, and Dave Blumberg. Catch Rooster Ridge playing shows in the greater Cincinnati area.
                              Please visit the event section or our Facebook page to stay up to date with future events!
-                             </p>
+                             </p> -->
                         </div>
                     </div>
 
