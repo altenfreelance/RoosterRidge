98ef7d88d89baa0e754dca533df1cf115103fb9a
andy <altenfreelance@gmail.com>
2020-01-07 19:45:56 -0500

    Style changes and updated gigs

--- src/Events.jsx
+++ src/Events.jsx
@@ -4,8 +4,8 @@ import Event from "./Event";
 
 export default function Events(){
 
-    const cappys_12_20 = "images/cappys_12_20.jpg";
-    const cappys_12_31 = "images/cappys_12_31.jpg";
+    // const cappys_12_20 = "images/cappys_12_20.jpg";
+    const arnolds = "images/arnolds.jpg";
     
 
     return (
@@ -13,19 +13,19 @@ export default function Events(){
         <h1>Upcoming Events</h1>
         <div className="row">
           <Event
-            name="Cappys Wine and Spirits"
-            date="12/20/2019"
-            time="8:00pm to 11:00pm"
-            address="309 W. Loveland Ave, Loveland, OH 45140"
-            img_url={cappys_12_20}
+            name="Arnolds Bar and Grill"
+            date="1/11/2020"
+            time="8:00pm to 11:30pm"
+            address="210 E 8th St, Cincinnati, OH 45202"
+            img_url={arnolds}
           />
-          <Event
+          {/* <Event
             name="New Year's Eve with Cappys Wine and Spirits"
             date="12/31/2019"
             time="8:00pm to 11:00pm"
             address="309 W. Loveland Ave, Loveland, OH 45140"
             img_url={cappys_12_31}
-          />
+          /> */}
         </div>
       </div>
     );
--- src/components/Songs.jsx
+++ src/components/Songs.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-const playerStyle = { backgroundColor: 'rgba(128,0,0, 0.8)' }
+const playerStyle = { backgroundColor: 'rgba(128,0,0, 0.8)', borderRadius: '30px' }
 
 function Songs() {
     return (
         <div>
             <h1>Audio</h1>
             {playlist.map((song) => {return (
-            <div key={song.title} className="my-2 mx-5 rounded p-4" style={playerStyle}>
+            <div key={song.title} className="my-2 mx-4 p-4" style={playerStyle}>
                 <h4 style={{ color: '' }}>{song.title}</h4>
                 <audio controls src={song.url} style={{ width: "100%" }} />
             </div>);
