7c397a9f8aa30fda56f24a451fc6edc0be29f737
andy <altenfreelance@gmail.com>
2020-02-27 21:41:48 -0500

    Removed march date (cancelled), added beech acres gig, added event sorter.

--- src/Event.jsx
+++ src/Event.jsx
@@ -12,17 +12,23 @@ export default class Event extends Component {
             <h4>
               <b>{name}</b>
             </h4>
+            
             <div style={{ color: "black" }}>
               <h5>{new Date(date).toDateString()}</h5>
               <h5>{time}</h5>
             </div>
+
             <Address address={address} />
-            <img
-              src={img_url}
-              alt={name}
-              className="border border-dark"
-              style={{ width: "100%"}}
-            />
+            <figure>
+              <figcaption className="figure-caption">{this.props.caption}</figcaption>
+              <img
+                src={img_url}
+                alt={name}
+                className="border border-dark"
+                style={{ width: "100%"}}
+                caption=""
+                />
+            </figure>
           </div>
         </div>
       </div>
--- src/Events.jsx
+++ src/Events.jsx
@@ -1,95 +1,117 @@
 import React from "react";
 import Event from "./Event";
 
+function sortEvents(events) {
+  
+  events.sort((i1, i2) => {
 
-export default function Events(){
+    let datei1 = new Date(i1.props.date);
+    let datei2 = new Date(i2.props.date);
 
-    const cappys1 = "images/cappys/cappys1.jpg";
-    const cappys2 = "images/cappys/cappys2.jpg";
-    const cappys3 = "images/cappys/cappys3.jpg";
-    const cappys4 = "images/cappys/cappys4.jpg";
-    const cappys5 = "images/cappys/cappys5.jpg";
+    if (datei1 < datei2) return -1;
+    if (datei1 > datei2) return 1;
+    return 0;
 
-    const arnolds = "images/arnolds/arnolds_us.jpg";
+  })
+  
+  return events;
+}
 
-    const downtown = "images/downtown/RailRoadAve.jpg";
+export default function Events() {
 
-    return (
-      <div>
-        <h1>Upcoming Events</h1>
-        <div className="row">
-          <Event
-            name="Bockfest with Arnolds Bar and Grill"
-            date="2/29/2020"
-            time="9:00pm to 12:30pm"
-            address="210 E 8th St, Cincinnati, OH 45202"
-            img_url={arnolds}
-          />
-          <Event
-            name="Cappys Wine and Spirits"
-            date="03/28/2020"
-            time="8:00pm"
-            address="309 W. Loveland Ave, Loveland, OH 45140"
-            img_url={cappys5}
-          />
-          <Event
-            name="Cappys Wine and Spirits"
-            date="05/16/2020"
-            time="8:00pm"
-            address="309 W. Loveland Ave, Loveland, OH 45140"
-            img_url={cappys1}
-          />
-           <Event
-            name="Rooster Ridge in Loveland"
-            date="06/28/2020"
-            time="6:00pm"
-            address="Downtown, Loveland, OH 45140"
-            img_url={downtown}
-          />
-          <Event
-            name="Independence Day with Cappys Wine and Spirits"
-            date="07/04/2020"
-            time="8:00pm"
-            address="309 W. Loveland Ave, Loveland, OH 45140"
-            img_url={cappys4}
-          />
-          <Event
-            name="Cappys Wine and Spirits"
-            date="08/15/2020"
-            time="8:00pm"
-            address="309 W. Loveland Ave, Loveland, OH 45140"
-            img_url={cappys3}
-          />
-          <Event
-            name="Cappys Wine and Spirits"
-            date="09/26/2020"
-            time="8:00pm"
-            address="309 W. Loveland Ave, Loveland, OH 45140"
-            img_url={cappys2}
-          />
-          <Event
-            name="Cappys Wine and Spirits"
-            date="10/24/2020"
-            time="8:00pm"
-            address="309 W. Loveland Ave, Loveland, OH 45140"
-            img_url={cappys5}
-          />
-          <Event
-            name="Thanksgiving Eve with Cappys Wine and Spirits"
-            date="11/25/2020"
-            time="8:00pm"
-            address="309 W. Loveland Ave, Loveland, OH 45140"
-            img_url={cappys1}
-          />
-          <Event
-            name="New Years Eve with Cappys Wine and Spirits"
-            date="12/31/2020"
-            time="8:00pm"
-            address="309 W. Loveland Ave, Loveland, OH 45140"
-            img_url={cappys2}
-          />
-        </div>
+  const cappys1 = "images/cappys/cappys1.jpg";
+  const cappys2 = "images/cappys/cappys2.jpg";
+  const cappys3 = "images/cappys/cappys3.jpg";
+  const cappys4 = "images/cappys/cappys4.jpg";
+  const cappys5 = "images/cappys/cappys5.jpg";
+
+  const arnolds = "images/arnolds/arnolds_us.jpg";
+
+  const downtown = "images/downtown/RailRoadAve.jpg";
+
+  const beachAcres = "images/beechAcresPark/Amphitheater.png";
+
+  const events = [
+    <Event
+      name="Rooster Ridge in the Park"
+      date="06/18/2020"
+      time="7:00pm"
+      address="6915 Beechmont Ave. and 6910 Salem Road."
+      caption="Join Rooster Ridge at Beech Acres Park for great music and fun!"
+      img_url={beachAcres}
+    />,
+    <Event
+      name="Cappys Wine and Spirits"
+      date="05/16/2020"
+      time="8:00pm"
+      address="309 W. Loveland Ave, Loveland, OH 45140"
+      img_url={cappys1}
+    />,
+    <Event
+    name="Bockfest with Arnolds Bar and Grill"
+    date="2/29/2020"
+    time="9:00pm to 12:30pm"
+    address="210 E 8th St, Cincinnati, OH 45202"
+    img_url={arnolds}
+  />,
+    <Event
+      name="Rooster Ridge in Loveland"
+      date="06/28/2020"
+      time="6:00pm"
+      address="Downtown, Loveland, OH 45140"
+      img_url={downtown}
+    />,
+    <Event
+      name="Independence Day with Cappys Wine and Spirits"
+      date="07/04/2020"
+      time="8:00pm"
+      address="309 W. Loveland Ave, Loveland, OH 45140"
+      img_url={cappys4}
+    />,
+    <Event
+      name="Cappys Wine and Spirits"
+      date="08/15/2020"
+      time="8:00pm"
+      address="309 W. Loveland Ave, Loveland, OH 45140"
+      img_url={cappys3}
+    />,
+    <Event
+      name="Cappys Wine and Spirits"
+      date="09/26/2020"
+      time="8:00pm"
+      address="309 W. Loveland Ave, Loveland, OH 45140"
+      img_url={cappys2}
+    />,
+    <Event
+      name="Cappys Wine and Spirits"
+      date="10/24/2020"
+      time="8:00pm"
+      address="309 W. Loveland Ave, Loveland, OH 45140"
+      img_url={cappys5}
+    />,
+    <Event
+      name="Thanksgiving Eve with Cappys Wine and Spirits"
+      date="11/25/2020"
+      time="8:00pm"
+      address="309 W. Loveland Ave, Loveland, OH 45140"
+      img_url={cappys1}
+    />,
+    <Event
+      name="New Years Eve with Cappys Wine and Spirits"
+      date="12/31/2020"
+      time="8:00pm"
+      address="309 W. Loveland Ave, Loveland, OH 45140"
+      img_url={cappys2}
+    />
+  ];
+
+  return (
+    <div>
+      <h1>Upcoming Events</h1>
+      <div className="row">
+        {sortEvents(events).map(event => <React.Fragment key={event.props.date}>{event}</React.Fragment>)}
       </div>
-    );
-  }
+    </div>
+  );
+}
 
