15d82143eef12faf31c012bbcffe80743e5aa0a4
andy <altenfreelance@gmail.com>
2020-02-27 21:50:50 -0500

    Added 's

--- src/Events.jsx
+++ src/Events.jsx
@@ -41,14 +41,14 @@ export default function Events() {
       img_url={beachAcres}
     />,
     <Event
-      name="Cappys Wine and Spirits"
+      name="Cappy's Wine and Spirits"
       date="05/16/2020"
       time="8:00pm"
       address="309 W. Loveland Ave, Loveland, OH 45140"
       img_url={cappys1}
     />,
     <Event
-    name="Bockfest with Arnolds Bar and Grill"
+    name="Bockfest with Arnold's Bar and Grill"
     date="2/29/2020"
     time="9:00pm to 12:30pm"
     address="210 E 8th St, Cincinnati, OH 45202"
@@ -62,42 +62,42 @@ export default function Events() {
       img_url={downtown}
     />,
     <Event
-      name="Independence Day with Cappys Wine and Spirits"
+      name="Independence Day with Cappy's Wine and Spirits"
       date="07/04/2020"
       time="8:00pm"
       address="309 W. Loveland Ave, Loveland, OH 45140"
       img_url={cappys4}
     />,
     <Event
-      name="Cappys Wine and Spirits"
+      name="Cappy's Wine and Spirits"
       date="08/15/2020"
       time="8:00pm"
       address="309 W. Loveland Ave, Loveland, OH 45140"
       img_url={cappys3}
     />,
     <Event
-      name="Cappys Wine and Spirits"
+      name="Cappy's Wine and Spirits"
       date="09/26/2020"
       time="8:00pm"
       address="309 W. Loveland Ave, Loveland, OH 45140"
       img_url={cappys2}
     />,
     <Event
-      name="Cappys Wine and Spirits"
+      name="Cappy's Wine and Spirits"
       date="10/24/2020"
       time="8:00pm"
       address="309 W. Loveland Ave, Loveland, OH 45140"
       img_url={cappys5}
     />,
     <Event
-      name="Thanksgiving Eve with Cappys Wine and Spirits"
+      name="Thanksgiving Eve with Cappy's Wine and Spirits"
       date="11/25/2020"
       time="8:00pm"
       address="309 W. Loveland Ave, Loveland, OH 45140"
       img_url={cappys1}
     />,
     <Event
-      name="New Years Eve with Cappys Wine and Spirits"
+      name="New Years Eve with Cappy's Wine and Spirits"
       date="12/31/2020"
       time="8:00pm"
       address="309 W. Loveland Ave, Loveland, OH 45140"
