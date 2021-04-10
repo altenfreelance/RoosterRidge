a90b2021e4a2ad0d29e3fe1cc5901e8aa1c1a73f
Andrew Alten <altenfreelance@gmail.com>
2021-04-10 10:55:11 -0400

    Working through cms, events are being pulled via json now

---
+++ events/beachAcres.md
@@ -0,0 +1,7 @@
+---
+title: Rooster Ridge in the Park
+gigDateTime: 2021-06-24 19:00:00 -0400
+address: 6915 Beechmont Ave. and 6910 Salem Road.
+caption: Join Rooster Ridge at Beech Acres Park for great music and fun!
+thumbnail: /images/uploads/Amphitheater.png
+---
---
+++ src/mdToJson.js
@@ -0,0 +1,19 @@
+const parser = require('md-yaml-json').default;
+// const parseToJSON = require('md-yaml-json').processFile;
+
+var path = require('path');
+var directoryPath = path.join(__dirname, '../events');
+const parsedDir = parser(path.resolve(__dirname, directoryPath));
+var jsonContent = JSON.stringify(parsedDir, null, 2);
+const fs = require('fs');
+
+
+
+fs.writeFile("src/components/Events/parsed-events.json", jsonContent, 'utf8', function (err) {
+    if (err) {
+        console.log("An error occured while writing JSON Object to File.");
+        return console.log(err);
+    }
+ 
+    console.log("JSON file has been saved.");
+});
