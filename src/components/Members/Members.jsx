3c656cf9ada167871f875482da718a6b99ebba30
Andrew Alten <altenfreelance@gmail.com>
2021-12-02 12:58:22 -0500

    Removed vince and joey

--- src/components/Members/Members.jsx
+++ src/components/Members/Members.jsx
@@ -6,8 +6,6 @@ function Members(){
     const jacob_img = 'images/members/Jacob.jpg'
     const bryce_img = 'images/members/Bryce.jpg'
     const greg_img = 'images/members/Greg.jpg'
-    const joey_img = 'images/members/Joey.jpg'
-    const vince_img = 'images/members/Vince.jpg'
     const jamie_img = 'images/members/Jamie.jpg'
     const bauer_img = 'images/members/OLAS.jpg'
 
@@ -19,8 +17,6 @@ function Members(){
                     <Member name="Jacob Alten" instrument="Vocal, Guitar, Percussion" img_url={jacob_img}/>
                     <Member name="Bryce Clawson" instrument="Vocals, Harmonica " img_url={bryce_img}/>
                     <Member name="Greg Stevens" instrument="Banjo, Guitar, Violin" img_url={greg_img}/>
-                    <Member name="Joey Oberholzer" instrument="Vocals, Percussion" img_url={joey_img}/>
-                    <Member name="Vince Stevens" instrument="Dobro" img_url={vince_img}/>
                     <Member name="Jamie Davidson" instrument="Bass, Vocals" img_url={jamie_img}/>
                     <Member name="Dave Bauer" instrument="Sound" img_url={bauer_img}/>
                 </div>
